1559fd0e575645715c244fe7bc5e36b511b2f795
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-15 20:41:04 -0600

    setup test file and guesedWords test

--- jotto-game/src/Congrats.test.js
+++ jotto-game/src/Congrats.test.js
@@ -4,37 +4,36 @@ import Congrats from './Congrats';
 import { findByTestAttr, checkProps } from './test/testUtils';
 
 /**
-* Factory function to create a ReactWrapper for the Congrats component.
-* @function setup
-* @param {object} testValues - contextValues specific to this setup.
-* @returns {ReactWrapper}
-*/
+ * Factory function to create a ReactWrapper for the Congrats component.
+ * @function setup
+ * @param {object} testValues - contextValues specific to this setup.
+ * @returns {ReactWrapper}
+ */
 
 const defaultProps = { success: false };
 
 const setup = (props = {}, state = null) => {
-	const setupProps = {...defaultProps, ...props};
-	const wrapper = shallow(<Congrats {...setupProps} />);
+  const setupProps = { ...defaultProps, ...props };
+  const wrapper = shallow(<Congrats {...setupProps} />);
 
-	if (state) {
-		wrapper.setState(state);
-	}
-	return wrapper;
-}
+  if (state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+};
 
 test('renders without error', () => {
-	const wrapper = setup({ success: false });
-	const component = findByTestAttr(wrapper, 'component-congrats');
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, 'component-congrats');
 
-	expect(component.length).toBe(1);
+  expect(component.length).toBe(1);
 });
 
 test('renders no text when `success` props is false', () => {
-	const wrapper = setup({ success: false });
-	const component = findByTestAttr(wrapper, 'component-congrats');
+  const wrapper = setup({ success: false });
+  const component = findByTestAttr(wrapper, 'component-congrats');
 
-	expect(component.text()).toBe('');
-
+  expect(component.text()).toBe('');
 });
 
 test('renders non-empty congrats message when `success` props is true', () => {
@@ -44,7 +43,7 @@ test('renders non-empty congrats message when `success` props is true', () => {
 });
 
 test('doest not throw warning with expected props', () => {
-	const expectedProps = { success: false };
+  const expectedProps = { success: false };
 
-	checkProps(Congrats, expectedProps);
+  checkProps(Congrats, expectedProps);
 });
---
+++ jotto-game/src/GuessedWords.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const GuessedWords = props => {
+  return <div />;
+};
+
+GuessedWords.propTypes = {};
+
+export default GuessedWords;
