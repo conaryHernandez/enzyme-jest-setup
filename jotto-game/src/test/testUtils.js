b1044ecf504a88470af9c2ba2001d91542895ebf
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-19 16:25:56 -0600

    testing with redux store

--- jotto-game/src/test/testUtils.js
+++ jotto-game/src/test/testUtils.js
@@ -1,11 +1,23 @@
 import checkPropTypes from 'check-prop-types';
 
+import rootReducer from '../store/reducers';
+import { createStore } from 'redux';
+
+export const storeFactory = initialState => {
+  return createStore(rootReducer, initialState);
+};
+
 export const findByTestAttr = (wrapper, val) => {
-	return wrapper.find(`[data-test="${val}"]`);
-}
+  return wrapper.find(`[data-test="${val}"]`);
+};
 
 export const checkProps = (component, conformingProps) => {
-	const propError = checkPropTypes(component.propTypes, conformingProps, 'prop', component.name);
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name
+  );
 
-	expect(propError).toBeUndefined();
-}
+  expect(propError).toBeUndefined();
+};
