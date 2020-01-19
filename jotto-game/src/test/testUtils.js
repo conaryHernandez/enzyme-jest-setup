e1f44a0a0a236c333a28e9193189cafe80feb794
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-19 17:25:18 -0600

    adding thunk and added guesswords reducer

--- jotto-game/src/test/testUtils.js
+++ jotto-game/src/test/testUtils.js
@@ -1,10 +1,14 @@
 import checkPropTypes from 'check-prop-types';
 
 import rootReducer from '../store/reducers';
-import { createStore } from 'redux';
+import { middlewares } from '../middlewares';
+import { createStore, applyMiddleware } from 'redux';
 
 export const storeFactory = initialState => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const findByTestAttr = (wrapper, val) => {
