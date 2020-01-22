0abd0c212edca53619a6a5f1ab30c0df3496db36
Conary <31629328+conaryHernandez@users.noreply.github.com>
2020-01-21 20:35:31 -0600

    app redux props

--- jotto-game/src/App.js
+++ jotto-game/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import './App.css';
 
 import GuessedWords from './components/GuessedWords/GuessedWords';
 import Congrats from './components/Congrats/Congrats';
+import Input from './components/Input/Input';
+
+import { getSecretWord } from './store/actions';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h1>Jotto</h1>
-        <Congrats success={true} />
-        <GuessedWords
-          guessedWords={[{ guessedWord: 'train', letterMatchCount: 3 }]}
-        />
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    success: state.success,
+    guessedWords: state.guessedWords,
+    secretWord: state.secretWord
+  };
+};
+
+export default connect(mapStateToProps, { getSecretWord })(App);
