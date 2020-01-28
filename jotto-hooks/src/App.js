7e91289e3f56e466e4bdddef8b6ef926d4d210fb
conaryh <conaryh@gmail.com>
2020-01-28 15:25:38 -0600

    testing congrats component

--- jotto-hooks/src/App.js
+++ jotto-hooks/src/App.js
@@ -2,23 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Input from './components/Input/Input';
+import LanguagePicker from './components/LanguagePicker/LanguagePicker';
 
 import hookActions from './actions/hookActions';
+import LanguageContext from './contexts/languageContext';
 
 function reducer(state, action) {
   switch (action.type) {
     case 'setSecretWord':
       return { ...state, secretWord: action.payload };
+    case 'setLanguage':
+      return { ...state, language: action.payload };
     default:
       throw new Error(`Invalid action type ${action.type}`);
   }
 }
 
 function App() {
-  const [state, dispatch] = React.useReducer(reducer, { secretWord: null });
+  const [state, dispatch] = React.useReducer(reducer, {
+    secretWord: null,
+    language: 'en'
+  });
 
   const setSecretWord = secretWord =>
     dispatch({ type: 'setSecretWord', payload: secretWord });
+  const setLanguage = language =>
+    dispatch({ type: 'setLanguage', payload: language });
 
   React.useEffect(() => {
     hookActions.getSecretWord(setSecretWord);
@@ -37,7 +46,11 @@ function App() {
 
   return (
     <div className="container" data-test="component-app">
-      <Input secretWord={state.secretWord} />
+      <h1>Jotto</h1>
+      <LanguageContext.Provider value={state.language}>
+        <LanguagePicker setLanguage={setLanguage} />
+        <Input secretWord={state.secretWord} />
+      </LanguageContext.Provider>
     </div>
   );
 }
--- jotto-hooks/src/components/Congrats/Congrats.test.js
+++ jotto-hooks/src/components/Congrats/Congrats.test.js
@@ -1,49 +1,53 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+
+import { findByTestAttr } from '../../test/testUtils';
 import Congrats from './Congrats';
-import { findByTestAttr, checkProps } from '../../test/testUtils';
+import languageContext from '../../contexts/languageContext';
+import successContext from '../../contexts/successContext';
 
 /**
- * Factory function to create a ReactWrapper for the Congrats component.
- * @function setup
- * @param {object} testValues - contextValues specific to this setup.
- * @returns {ReactWrapper}
- */
+* Factory function to create a ReactWrapper for the Congrats component.
+* @function setup
+* @param {object} testValues - contextValues specific to this setup.
+* @returns {ReactWrapper}
+*/
+const setup = ({ success, language }) => {
+  language = language || 'en';
+  success = success || false;
 
-const defaultProps = { success: false };
-
-const setup = (props = {}, state = null) => {
-  const setupProps = { ...defaultProps, ...props };
-  const wrapper = shallow(<Congrats {...setupProps} />);
-
-  if (state) {
-    wrapper.setState(state);
-  }
-  return wrapper;
+  return mount(
+    <languageContext.Provider value={language}>
+      <successContext.SuccessProvider value={[success, jest.fn()]}>
+        <Congrats />
+      </successContext.SuccessProvider>
+    </languageContext.Provider>
+  );
 };
 
-test('renders without error', () => {
-  const wrapper = setup({ success: false });
-  const component = findByTestAttr(wrapper, 'component-congrats');
+describe('language picker', () => {
+  test('correctly renders congrats string in English by default', () => {
+    const wrapper = setup({ success: true });
+    expect(wrapper.text()).toBe('Congratulations! You guessed the word!');
+  });
+  test('correctly renders congrats string in emoji', () => {
+    const wrapper = setup({ success: true, language: 'emoji' });
+    expect(wrapper.text()).toBe('🎯🎉');
+  });
+});
 
+test('renders without error', () => {
+  const wrapper = setup({});
+  const component = findByTestAttr(wrapper, 'component-congrats');
   expect(component.length).toBe(1);
 });
-
-test('renders no text when `success` props is false', () => {
+test('renders no text when `success` is false', () => {
   const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, 'component-congrats');
-
   expect(component.text()).toBe('');
 });
-
-test('renders non-empty congrats message when `success` props is true', () => {
+test('renders non-empty congrats message when `success` is true', () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
-});
-
-test('doest not throw warning with expected props', () => {
-  const expectedProps = { success: false };
-
-  checkProps(Congrats, expectedProps);
 });
